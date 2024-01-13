79bfbd2ba751e69413d8fa87d7bc08e957b85c77
Maroš Pekárik <maros.pekarik@pm.me>
2024-01-13 14:27:18 +0100

    switch to a simple blog theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,81 +1,21 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
-import { defineConfig, squooshImageService } from 'astro/config';
-
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
-import icon from 'astro-icon';
-import tasks from './src/utils/tasks';
-
-import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin } from './src/utils/frontmatter.mjs';
-
-import { ANALYTICS, SITE } from './src/utils/config.ts';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-const whenExternalScripts = (items = []) =>
-  ANALYTICS.vendors.googleAnalytics.id && ANALYTICS.vendors.googleAnalytics.partytown
-    ? Array.isArray(items)
-      ? items.map((item) => item())
-      : [items()]
-    : [];
-
+// https://astro.build/config
 export default defineConfig({
-  site: SITE.site,
-  base: SITE.base,
-  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
-
-  output: 'static',
-
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-    mdx(),
-    icon({
-      include: {
-        tabler: ['*'],
-        'flat-color-icons': [
-          'template',
-          'gallery',
-          'approval',
-          'document',
-          'advertising',
-          'currency-exchange',
-          'voice-presentation',
-          'business-contact',
-          'database',
-        ],
-      },
-    }),
-
-    ...whenExternalScripts(() =>
-      partytown({
-        config: { forward: ['dataLayer.push'] },
-      })
-    ),
-
-    tasks(),
-  ],
-
-  image: {
-    service: squooshImageService(),
-  },
-
   markdown: {
-    remarkPlugins: [readingTimeRemarkPlugin],
-    rehypePlugins: [responsiveTablesRehypePlugin],
+    drafts: true,
+    shikiConfig: {
+      theme: "css-variables"
+    }
   },
-
-  vite: {
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, './src'),
-      },
-    },
+  shikiConfig: {
+    wrap: true,
+    skipInline: false,
+    drafts: true
   },
+  site: 'https://lexingtonthemes.com',
+  integrations: [tailwind(), sitemap(), mdx()]
 });
